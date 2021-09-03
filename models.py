6ddcbc05516886dbfe6acd1bd19a5f9aceada23a
Bradul Dmytro <dbradul@gmail.com>
2021-09-03 10:46:26 +0300

    Save current DB state

--- models.py
+++ models.py
@@ -7,3 +7,10 @@ class GentlemanInfo(BaseModel):
     profile_id: Optional[int] = 0
     age_from: Optional[int] = 0
     age_to: Optional[int] = 0
+
+
+# class RunStats(BaseModel):
+#     is_online: int = 0
+#     rejected_age_mismatch: int = 0
+#     letters_sent: int = 0
+#     country: str
